acd5838dcd12dd7ee822a8d512801a84cf1913f6
teru0707 <ro23089l@st.omu.ac.jp>
2026-01-22 15:08:40 +0900

    forth commit

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -18,7 +18,7 @@ const Header: React.FC = () => {
           <div>
             <Link href="/">
               <FontAwesomeIcon icon={faFish} className="mr-1" />
-              Header
+              てるきのブログ
             </Link>
           </div>
           <div>
